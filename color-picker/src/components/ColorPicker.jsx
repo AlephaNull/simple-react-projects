025b20d897a856e0e1dd0685a6ac868fa326cfac
Alepha Null <HashTag>
2024-04-28 20:48:55 +0530

    Completed color picker proj

--- color-picker/src/components/ColorPicker.jsx
+++ color-picker/src/components/ColorPicker.jsx
@@ -4,16 +4,24 @@ import '../index.css'
 
 const colorPicker = () => {
   const [color, setColor] = useState('#ffffff')
+
+  const handleColorChange = () => {
+    setColor(event.target.value)
+  }
   return (
     <div>
-      <div className="color-display">
+      <div className="color-display" style={{
+        backgroundColor: color,
+      }}>
         <h2> Selected Color </h2>
-        <span> {color} </span>
+        <span style={{
+          fontSize: "1.5em",
+        }}> {color} </span>
       </div>
 
       <div className="color-picker">
         <label> Select a color </label>
-        <input type='color' value={color} />
+        <input type='color' value={color} onChange={handleColorChange} />
       </div>
     </div>
   )
